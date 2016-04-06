041362dfae00403b95d94fd92b8999c4e87ea3b2
wuyunxia <462942967@qq.com>
2016-04-06 16:15:19 +0800

    add PreviewImg

--- statics/js/products/product.js
+++ statics/js/products/product.js
@@ -18,6 +18,9 @@ var date_to = '';
 var order = '';
 var desc = false;
 var product_id = 0;
+
+var previewImgId = -1;
+
 //产品分类 绑定
 $('select[name=product1]').bind('change', function () {
     $('select[name=product2]').html('<option value="0">请选择</option>');
@@ -146,10 +149,14 @@ $('.js-product-detail').on('click', function () {
             var file = data.files[index];
             $('.js-file-list').append('<div data-id="'+file.id+'">'+file.name+'</div>');
         }
+        var tmp = 0;
         for(var index in data.previews){
+            tmp++;
             var preview = data.previews[index];
-            $('.js-preview-list').append('<div data-id="'+preview.id+'"><a href="'+preview.url+'" target="_blank"><img class="inline-table product-info-box-img" src="'+preview.url+'"/></a></div>');
+            $('.js-preview-list').append('<div data-id="'+preview.id+'" data-url="'+preview.url+'"><img class="inline-table product-info-box-img" src="'+preview.url+'"/></div>');
+            //$('.js-preview-list').append('<div data-id="'+preview.id+'"><a href="'+preview.url+'" target="_blank"><img class="inline-table product-info-box-img" src="'+preview.url+'"/></a></div>');
         }
+        $('.js-preview-list').css('width', tmp * 110);
         $('.js-modal-product-remarks').html(data.remarks);
     }, 'JSON');
 });
@@ -172,44 +179,70 @@ $('.js-void').on('click', function () {
     $(this).parent().find('.js-void').hide();
 });
 
-var upload_model_file = function() {
-    var data = new FormData();
-    data.append('file', document.getElementById('model-file').files[0]);
-    data.append('product_id', product_id);
-    data.append('csrfmiddlewaretoken', $('input[name="csrfmiddlewaretoken"]').val());
-    $.ajax({
-        type: "POST",
-        url: "/products/product/file/upload/",
-        data: data,
-        cache: false,
-        dataType: 'json',
-        processData: false, // Don't process the files
-        contentType: false, // Set content type to false as jQuery will tell the server its a query string request
-        success: function (data) {
-            $('.js-file-list').append('<div data-id="'+data.id+'">'+data.name+'</div>');
-        }
-    });
-}
+//var upload_model_file = function() {
+//    var data = new FormData();
+//    data.append('file', document.getElementById('model-file').files[0]);
+//    data.append('product_id', product_id);
+//    data.append('csrfmiddlewaretoken', $('input[name="csrfmiddlewaretoken"]').val());
+//    $.ajax({
+//        type: "POST",
+//        url: "/products/product/file/upload/",
+//        data: data,
+//        cache: false,
+//        dataType: 'json',
+//        processData: false, // Don't process the files
+//        contentType: false, // Set content type to false as jQuery will tell the server its a query string request
+//        success: function (data) {
+//            $('.js-file-list').append('<div data-id="'+data.id+'">'+data.name+'</div>');
+//        }
+//    });
+//};
+//
+//var upload_model_preview = function() {
+//    var data = new FormData();
+//    data.append('file', document.getElementById('model-preview').files[0]);
+//    data.append('product_id', product_id);
+//    data.append('csrfmiddlewaretoken', $('input[name="csrfmiddlewaretoken"]').val());
+//    $.ajax({
+//        type: "POST",
+//        url: "/products/product/preview/upload/",
+//        data: data,
+//        cache: false,
+//        dataType: 'json',
+//        processData: false, // Don't process the files
+//        contentType: false, // Set content type to false as jQuery will tell the server its a query string request
+//        success: function (data) {
+//            $('.js-preview-list').append('<div data-id="'+data.id+'"><a href="'+data.url+'" target="_blank"><img class="inline-table product-info-box-img" src="'+data.url+'"/></a></div>');
+//            $('img[data-id="'+product_id+'"]').attr('src',data.url);
+//        }
+//    });
+//};
 
-var upload_model_preview = function() {
-    var data = new FormData();
-    data.append('file', document.getElementById('model-preview').files[0]);
-    data.append('product_id', product_id);
-    data.append('csrfmiddlewaretoken', $('input[name="csrfmiddlewaretoken"]').val());
-    $.ajax({
-        type: "POST",
-        url: "/products/product/preview/upload/",
-        data: data,
-        cache: false,
-        dataType: 'json',
-        processData: false, // Don't process the files
-        contentType: false, // Set content type to false as jQuery will tell the server its a query string request
-        success: function (data) {
-            $('.js-preview-list').append('<div data-id="'+data.id+'"><a href="'+data.url+'" target="_blank"><img class="inline-table product-info-box-img" src="'+data.url+'"/></a></div>');
-            $('img[data-id="'+product_id+'"]').attr('src',data.url);
+var loadPreviewImg = function(){
+    var tmp = $(this).attr('data-url');
+    $('div[data-name=previewImg] img').attr('src', tmp);
+    $('div[data-name=previewImg]').show();
+    previewImgId = eval($(this).attr('data-id'));
+    console.log(previewImgId + '-----')
+};
+
+var changePreviewImg = function(){
+    if($(this).attr('data-name') == 'previewImgLeft'){
+        if(previewImgId == $('div[data-name=preview-box] .js-preview-list div[data-id]').length) {
+            previewImgId = 1;
+        } else {
+            previewImgId++;
         }
-    });
-}
+    } else if ($(this).attr('data-name') == 'previewImgRight'){
+        if(previewImgId == 1) {
+            previewImgId = $('div[data-name=preview-box] .js-preview-list div[data-id]').length;
+        } else {
+            previewImgId--;
+        }
+    }
+    var tmp = $('.js-preview-list div[data-id=' + previewImgId + ']').attr('data-url');
+    $('div[data-name=previewImg] img').attr('src', tmp);
+};
 
 var productApp = function () {
     return {
@@ -245,12 +278,22 @@ var productApp = function () {
             $('.js-modal-upload-file').on('click', function () {
                 $('input[name="model-file"]').click();
             });
-            $('input[name="model-file"]').on('change', upload_model_file);
+            //$('input[name="model-file"]').on('change', upload_model_file);
             $('.js-modal-upload-preview').on('click', function () {
                 $('input[name="model-preview"]').click();
                 console.log($('input[name="model-preview"]'))
             });
-            $('input[name="model-preview"]').on('change', upload_model_preview);
-        },
+            //$('input[name="model-preview"]').on('change', upload_model_preview);
+
+            $(document).on('dblclick', '.modal-body .js-preview-list>div', loadPreviewImg);
+            $('div[data-name=previewImg]').on('click', function(){$(this).hide()});
+            $('div[data-name=previewImg]>div').hover(function(){
+                $('div[data-name=previewImg]').off('click');
+                $(document).on('click', 'div[data-name=previewImg]>div', changePreviewImg);
+            },function(){
+                $('div[data-name=previewImg]').on('click', function(){$(this).hide()});
+                $(document).off('click', 'div[data-name=previewImg]>div', changePreviewImg);
+            });
+        }
     }
 }();
