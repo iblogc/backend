378cc82b9204aac9ad5f923a3f0b64b95fe32ae4
gaodi <14841787@qq.com>
2016-04-11 13:17:05 +0800

    customer views and htmls

--- customers/urls.py
+++ customers/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
-from customers.views import AccountViews
+from customers.views import AccountViews, CertifyViews, ApproveViews, LogViews
 
 urlpatterns = [
     url(r'accounts/$', AccountViews.as_view(), name='accounts'),
+    url(r'certifies/$', CertifyViews.as_view(), name='certifies'),
+    url(r'approves/$', ApproveViews.as_view(), name='approves'),
+    url(r'logs/$', LogViews.as_view(), name='logs'),
 ]
